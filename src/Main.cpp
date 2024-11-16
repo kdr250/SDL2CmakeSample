2280df521c656769e85681f7393a476801c47f4a
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-11-16 19:12:26 +0900

    エラー対応

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,77 +1,68 @@
-#include <iostream>
 #include <SDL2/SDL.h>
 
 int main(int argc, char* args[])
 {
-    const int SCREEN_WIDTH = 800;
-    const int SCREEN_HEIGHT = 600;
-
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        std::cout << "SDL init failed." << std::endl;
-        return 1;
+    int sdlResult = SDL_Init(SDL_INIT_VIDEO);
+    if (sdlResult != 0)
+    {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return false;
     }
 
-    // initialize screen
-    SDL_Window* window = SDL_CreateWindow("Hello world !!", SDL_WINDOWPOS_UNDEFINED,
-        SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    auto window = SDL_CreateWindow("Hello world !!",  // window title
+                                   100,               // Top left x-coordinate of window
+                                   100,               // Top left y-coordinate of window
+                                   1024,              // width of window
+                                   768,               // height of window
+                                   0                  // flags (0 for no flags set)
+    );
 
-    if (window == NULL) {
-        SDL_Quit();
-        return 2;
+    if (!window)
+    {
+        SDL_Log("Failed to create window: %s", SDL_GetError());
+        return false;
     }
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
-    SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
-        SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+    auto renderer = SDL_CreateRenderer(window,  // window to create renderer for
+                                       -1,      // usually -1
+                                       SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-    if (renderer == NULL) {
-        std::cout << "Could not create renderer" << std::endl;
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 3;
+    if (!renderer)
+    {
+        SDL_Log("Failed to create renderer: %s", SDL_GetError());
+        return false;
     }
 
-    if (texture == NULL) {
-        std::cout << "Could not create texture" << std::endl;
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 4;
-    }
+    bool isRunning = true;
 
-    // prepare memory
-    Uint32* buffer = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
-
-    memset(buffer, 0, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
-
-    // set every 4bytes as color into memory
-    for (int i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT; i++) {
-        buffer[i] = 0x008080FF;
-    }
-
-    // update screen
-    SDL_UpdateTexture(texture, NULL, buffer, SCREEN_WIDTH * sizeof(Uint32));
-    SDL_RenderClear(renderer);
-    SDL_RenderCopy(renderer, texture, NULL, NULL);
-    SDL_RenderPresent(renderer);
-
-    bool quit = false;
-    SDL_Event event;
-
-    while (!quit) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                quit = true;
-            };
+    while (isRunning)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+                // If we get an SDL_QUIT event, end loop
+                case SDL_QUIT:
+                    isRunning = false;
+                    break;
+            }
         }
-    }
 
-    // finalize screen
-    delete[] buffer;
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyTexture(texture);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+        // set draw color blue
+        SDL_SetRenderDrawColor(renderer,
+                               0,    // R
+                               0,    // G
+                               255,  // B
+                               255   // A
+        );
+
+        // clear back buffer
+        SDL_RenderClear(renderer);
+
+        // exchange front buffer for back buffer
+        SDL_RenderPresent(renderer);
+    }
 
     return 0;
 }
